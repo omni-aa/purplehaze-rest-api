b1c5ba1f405f7a841cea9d3de90b9a9c8b3d2ecf
omni <aidanali65@gmail.com>
2025-12-24 13:48:48 -0500

    converting project from express to fastify added authRoutes.ts, dashboardRoutes.ts

---
+++ src/routes/authRoutes/authRoutes.ts
@@ -0,0 +1,20 @@
+﻿import {FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
+
+
+export async function authRoutes(fastify:FastifyInstance) {
+
+    fastify.post('/sign-up', async (request: FastifyRequest,reply:FastifyReply)=>{
+
+    })
+
+
+    fastify.post('/sign-in', async (request: FastifyRequest,reply:FastifyReply)=>{
+
+    })
+    fastify.post('/password-reset-request', async (request: FastifyRequest,reply:FastifyReply)=>{
+
+    })
+    fastify.post('/password-reset', async (request: FastifyRequest,reply:FastifyReply)=>{
+
+    })
+}
---
+++ src/routes/dashboardRoutes/dashboardRoutes.ts
@@ -0,0 +1,14 @@
+﻿import {FastifyInstance, FastifyReply, FastifyRequest} from "fastify";
+
+
+export async function dashboardRoutes(fastify:FastifyInstance) {
+
+    fastify.post('/profile', async (request: FastifyRequest,reply:FastifyReply)=>{
+
+    })
+
+
+    fastify.post('/stats', async (request: FastifyRequest,reply:FastifyReply)=>{
+
+    })
+}
